814f4a1a5b681ecb8538fa62ea0b850e4016ea4c
ElGuardianDelJardin <47043260+ElGuardianDelJardin@users.noreply.github.com>
2021-03-12 20:04:56 +0100

    Particles Portfolio
    for adv class

---
+++ Assets/0_Lesson 4/Shader/waterSplash.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterSplash : MonoBehaviour
+{
+    public GameObject SplashInPrefab;
+    public GameObject SplashOutPrefab;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Instantiate(SplashInPrefab, collision.ClosestPointOnBounds(collision.attachedRigidbody.position),Quaternion.identity);
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        Instantiate(SplashOutPrefab, collision.ClosestPointOnBounds(collision.attachedRigidbody.position), Quaternion.identity);
+    }
+}
---
+++ Assets/0_Lesson 4/waterPushing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waterPushing : MonoBehaviour
+{
+    public Vector3 pushForce;
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.GetComponent<Rigidbody>())
+            other.GetComponent<Rigidbody>().AddForce(pushForce * Time.deltaTime,ForceMode.Force);
+    }
+    
+}
