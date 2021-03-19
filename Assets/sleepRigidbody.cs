422fb7a3ad9aff8ad9b6cb09e0f5ece13afb51f2
ElGuardianDelJardin <47043260+ElGuardianDelJardin@users.noreply.github.com>
2021-03-19 16:49:41 +0100

    not ended audio issues

---
+++ Assets/sleepRigidbody.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sleepRigidbody : MonoBehaviour
+{
+    private Rigidbody rbGO;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Rock")
+            rbGO.WakeUp();
+    }
+
+    void Start()
+    {
+        rbGO = gameObject.GetComponent<Rigidbody>();
+        rbGO.Sleep();
+    }
+
+   
+}
+
