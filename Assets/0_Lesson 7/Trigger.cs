45b0e17a1938839379f2dc8d95f999094f3c21ad
ElGuardianDelJardin <47043260+ElGuardianDelJardin@users.noreply.github.com>
2021-03-26 20:09:18 +0100

    Lesson7 - State Machines
    Full lesson7- portfolio

---
+++ Assets/0_Lesson 7/Trigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public Animator objectToAnimate;
+    public string animatorVariable;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player" || other.tag == "MainCamera")
+            objectToAnimate.SetBool(animatorVariable, true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "MainCamera")
+            objectToAnimate.SetBool(animatorVariable, false);
+    }
+}
