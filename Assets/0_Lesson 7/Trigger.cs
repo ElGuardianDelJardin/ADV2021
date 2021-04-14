0fdde70f9c2462c6e6274dd8d7a14d60048b3c79
ElGuardianDelJardin <47043260+ElGuardianDelJardin@users.noreply.github.com>
2021-04-14 21:51:39 +0200

    project3 state machine and blend tree
    from the adv course at UPV

--- Assets/0_Lesson 7/Trigger.cs
+++ Assets/0_Lesson 7/Trigger.cs
@@ -1,21 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Trigger : MonoBehaviour
 {
     public Animator objectToAnimate;
     public string animatorVariable;
 
+    public Text texto;
+
+    private bool open;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player" || other.tag == "MainCamera")
-            objectToAnimate.SetBool(animatorVariable, true);
+        if (other.tag == "Player" || other.tag == "MainCamera")
+        {
+            objectToAnimate.SetBool("Trigger", true);
+            
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "MainCamera")
+            objectToAnimate.SetBool(animatorVariable, open);
+
+        if (objectToAnimate.GetBool(animatorVariable)) {
+            if (texto)
+                texto.text = "press Space to close chest"; }
+        else {
+            if (texto)
+                texto.text = "press Space to open chest"; }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player" || other.tag == "MainCamera")
+        {
             objectToAnimate.SetBool(animatorVariable, false);
+            objectToAnimate.SetBool("Trigger", false);
+            if(texto)
+            texto.text = "";
+
+
+        }
     }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            open = !open;
+    }
+
 }
---
+++ Assets/0_Lesson 7/move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public Animator anim;
+    public float speed;
+    public float turn;
+    public float maxSpeed;
+
+    public float StopVelocityForce;
+
+    Vector3 localVelocity;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        localVelocity.z = gameObject.GetComponent<Rigidbody>().velocity.z;
+        localVelocity.y = gameObject.GetComponent<Rigidbody>().velocity.y;
+        localVelocity.x = gameObject.GetComponent<Rigidbody>().velocity.x - StopVelocityForce;
+
+        Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+        localVelocity = rb.transform.InverseTransformDirection(rb.velocity);
+        localVelocity.x *= 0.5f; // cut sideways speed in half
+        rb.velocity = rb.transform.TransformDirection(localVelocity);
+
+
+        if (Mathf.Abs(localVelocity.z) < maxSpeed)
+        gameObject.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0 ,Input.GetAxis("Vertical")) * speed);
+        gameObject.transform.Rotate(new Vector3(0, Input.GetAxis("Horizontal"), 0 ) * turn);
+
+        anim.SetFloat("Blend", (Input.GetAxis("Vertical") + 1) / 2);
+        anim.speed = Mathf.Abs(localVelocity.z) / 20;
+    }
+}
