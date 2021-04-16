fe2b8e9ab6804c5b9528b5a1066ebace74caf290
ElGuardianDelJardin <47043260+ElGuardianDelJardin@users.noreply.github.com>
2021-04-16 21:06:23 +0200

    portfolio Character/morph animation
    part of the adv course

---
+++ Assets/line.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class line : MonoBehaviour
+{
+    public GameObject target1;
+    public GameObject target2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<LineRenderer>().SetPosition(0, target1.transform.position);
+        gameObject.GetComponent<LineRenderer>().SetPosition(1, target2.transform.position);
+    }
+}
