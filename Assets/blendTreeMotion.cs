12ae65960d10a318c9123ea8699e6944bc2f4ca8
ElGuardianDelJardin <47043260+ElGuardianDelJardin@users.noreply.github.com>
2021-04-23 21:00:47 +0200

    Portfolio Blendtree 2D
    from the ADV course

---
+++ Assets/blendTreeMotion.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blendTreeMotion : MonoBehaviour
+{
+    private Animator anim;
+    private CharacterController charControl;
+
+    public float gravity = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        charControl = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        anim.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        anim.SetFloat("Vertical", Input.GetAxis("Vertical"));
+
+        if (Input.GetKey(KeyCode.LeftShift)) 
+        {
+            anim.SetBool("Run", true);
+        }
+        else 
+        {
+            anim.SetBool("Run", false);
+        }
+
+
+    }
+}
